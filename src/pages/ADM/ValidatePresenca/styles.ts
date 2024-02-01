62c79d94bd8c542c92affc545d8c70b71af58a86
app-c <william@app-com.digital>
2024-02-01 08:48:18 -0300

    .

--- src/pages/ADM/ValidatePresenca/styles.ts
+++ src/pages/ADM/ValidatePresenca/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${h => h.theme.colors.bg_color[1]}
 `;
 
 export const Title = styled.Text``;
